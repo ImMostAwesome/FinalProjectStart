8ce6af3d23f4b062d8772793138c1d774a0509b4
ImMostAwesome <74273445+ImMostAwesome@users.noreply.github.com>
2020-11-10 20:07:09 -0700

    Updated

--- jScript/toActivate.js
+++ jScript/toActivate.js
@@ -12,6 +12,12 @@ function highlight(event){
 function unHighlight(event){
     event.target.style = "box-shadow: none;"
 }
+let sideBar={
+    Home:"Click on the links to see more",
+    Projects:"These are my favorite projects I've worked on over the years",
+    About_Me:"Now you know everything there is to know",
+    HomeTown:"This is the best place on Earth!!"
+}
 let shown = "Home";
 function linkClicked(ev){
     for (let toShow of document.getElementsByClassName(shown)) {
@@ -20,6 +26,7 @@ function linkClicked(ev){
     for (let toShow of document.getElementsByClassName(ev.target.innerHTML)) {
         toShow.style.display = "flex";
     }
+    document.getElementById("related-content").innerHTML = sideBar[ev.target.innerHTML];
     document.getElementById("title").innerHTML = "<h1 style=\"padding-left:50px;\">"+ ev.target.innerHTML +"</h1>"
     let temp = ev.target.innerHTML;
     ev.target.innerHTML = shown;
