722c36070c9147714222947e000d95fbcc6fe300
ImMostAwesome <74273445+ImMostAwesome@users.noreply.github.com>
2020-11-10 19:09:54 -0700

    clean

---
+++ jScript/toActivate.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded", function()
+{
+    for(let button of document.getElementsByClassName("inter")){
+        button.addEventListener("mouseover", highlight);
+        button.addEventListener("mouseout", unHighlight);
+    }
+	
+});
+function highlight(event){
+    event.target.style = "box-shadow: 0 0 10px blue;"
+}
+function unHighlight(event){
+    event.target.style = "box-shadow: none;"
+}
+let shown = "Home";
+function linkClicked(ev){
+    for (let toShow of document.getElementsByClassName(shown)) {
+        toShow.style.display = "none";
+    } 
+    for (let toShow of document.getElementsByClassName(ev.target.innerHTML)) {
+        toShow.style.display = "flex";
+    }
+    document.getElementById("title").innerHTML = "<h1 style=\"padding-left:50px;\">"+ ev.target.innerHTML +"</h1>"
+    let temp = ev.target.innerHTML;
+    ev.target.innerHTML = shown;
+    shown = temp;
+}
